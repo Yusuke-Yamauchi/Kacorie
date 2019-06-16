7936b0f5f841546df4c82f396a93a1a2c247a2bc
Yusuke-Yamauchi <yusan823@gmail.com>
2019-06-16 23:12:35 +0900

    firstcommit

--- Kacorie/ViewController.swift
+++ Kacorie/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import IBAnimatable
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
-}
 
+}
